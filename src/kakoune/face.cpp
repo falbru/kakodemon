14336d842a8b8a5697b1f6f05a75c14266c3bd06
Falk Bruskeland <falk.bruskeland@gmail.com>
2025-07-28 20:17:18 +0200

    feat: add attributes

--- src/kakoune/face.cpp
+++ src/kakoune/face.cpp
@@ -1,12 +1,51 @@
 #include "face.hpp"
 
+void kakoune::to_json(nlohmann::json &j, const Attribute &a)
+{
+    switch (a) {
+        case UNDERLINE:        j = "underline"; break;
+        case CURLY_UNDERLINE:  j = "curly_underline"; break;
+        case DOUBLE_UNDERLINE: j = "double_underline"; break;
+        case REVERSE:          j = "reverse"; break;
+        case BLINK:            j = "blink"; break;
+        case BOLD:             j = "bold"; break;
+        case DIM:              j = "dim"; break;
+        case ITALIC:           j = "italic"; break;
+        case FINAL_FG:         j = "final_fg"; break;
+        case FINAL_BG:         j = "final_bg"; break;
+        case FINAL_ATTR:       j = "final_attr"; break;
+    }
+}
+
+void kakoune::from_json(const nlohmann::json &j, Attribute &a)
+{
+    std::string attr_str = j.get<std::string>();
+
+    if (attr_str == "underline")             a = UNDERLINE;
+    else if (attr_str == "curly_underline")  a = CURLY_UNDERLINE;
+    else if (attr_str == "double_underline") a = DOUBLE_UNDERLINE;
+    else if (attr_str == "reverse")          a = REVERSE;
+    else if (attr_str == "blink")            a = BLINK;
+    else if (attr_str == "bold")             a = BOLD;
+    else if (attr_str == "dim")              a = DIM;
+    else if (attr_str == "italic")           a = ITALIC;
+    else if (attr_str == "final_fg")         a = FINAL_FG;
+    else if (attr_str == "final_bg")         a = FINAL_BG;
+    else if (attr_str == "final_attr")       a = FINAL_ATTR;
+    else {
+        throw nlohmann::json::parse_error::create(
+            101, 0, "Invalid attribute value: " + attr_str, nullptr);
+    }
+}
+
 void kakoune::to_json(nlohmann::json &j, const Face &f)
 {
-    j = nlohmann::json{{"fg", f.fg}, {"bg", f.bg}};
+    j = nlohmann::json{{"fg", f.fg}, {"bg", f.bg}, {"attributes", f.attributes}};
 }
 
 void kakoune::from_json(const nlohmann::json &j, Face &f)
 {
     j.at("fg").get_to(f.fg);
     j.at("bg").get_to(f.bg);
+    j.at("attributes").get_to(f.attributes);
 }
--- src/kakoune/face.hpp
+++ src/kakoune/face.hpp
@@ -7,14 +7,33 @@
 namespace kakoune
 {
 
+enum Attribute
+{
+    UNDERLINE,
+    CURLY_UNDERLINE,
+    DOUBLE_UNDERLINE,
+    REVERSE,
+    BLINK,
+    BOLD,
+    DIM,
+    ITALIC,
+    FINAL_FG,
+    FINAL_BG,
+    FINAL_ATTR
+};
+
 struct Face
 {
     Color fg;
     Color bg;
+    std::vector<Attribute> attributes;
 };
 
 void to_json(nlohmann::json &j, const Face &f);
 void from_json(const nlohmann::json &j, Face &f);
+
+void to_json(nlohmann::json &j, const Attribute &a);
+void from_json(const nlohmann::json &j, Attribute &a);
 
 } // namespace kakoune
 
